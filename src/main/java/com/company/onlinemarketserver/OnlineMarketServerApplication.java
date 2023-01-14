7405a86333300a2ff351e424229ed27c82914878
SardorbekJavaDev <sardorbekyorqulov1@gmail.com>
2023-01-14 12:16:06 +0500

    project opened

---
+++ src/main/java/com/company/onlinemarketserver/OnlineMarketServerApplication.java
@@ -0,0 +1,13 @@
+package com.company.onlinemarketserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineMarketServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineMarketServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/company/onlinemarketserver/OnlineMarketServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.company.onlinemarketserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineMarketServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
